794310959df4efc08a4157fc6faec3b8fdfd5b40
Alexandra <chrome9600@gmail.com>
2018-09-04 23:20:11 +0300

    Add work 1

--- README.md
+++
@@ -1,1 +0,0 @@
-ifmo_programming
---
+++ work1_task3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <clocale>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, ""); // подключение русского языка
+    cout << "Введите периметр треугольника: ";
+    double perimeter, side, side2, semiperimeter, square;
+    cin >> perimeter; // пользователь вводит периметр треугольника
+    side = perimeter / 3; // сторона
+    semiperimeter = perimeter / 2; // полупериметр
+    side2 = semiperimeter - side; // для упрощения выражения заменим на одну переменную
+    square = sqrt(semiperimeter * pow(side2, 3)); // вычисление площади
+    cout << endl << setw(10) << left << "Сторона" << setw(10) << left << "Площадь"; // вывод результата
+    cout << endl << setw(10) << left << side << setw(10) << left << square;
+    return 0;
+}
