92a98434da4bbf45db8067d764c43d4b936e69cb
Alexandra <chrome9600@gmail.com>
2018-09-06 20:27:19 +0300

    Fix output formatting in Unix

--- work1_task3/main.cpp
+++ work1_task3/main.cpp
@@ -5,17 +5,15 @@
 
 using namespace std;
 
-int main()
-{
-    setlocale(0, "");  // подключение русского языка
-    cout << "Введите периметр треугольника: ";
-    double perimeter, side, side2, semiperimeter, square;
+int main() {
+    setlocale(LC_ALL, ""); // подключение русского языка
+    wcout << L"Введите периметр треугольника: ";
+    double perimeter;
     cin >> perimeter; // пользователь вводит периметр треугольника
-    side = perimeter / 3; // сторона
-    semiperimeter = perimeter / 2; // полупериметр
-    side2 = semiperimeter - side; // для упрощения выражения заменим на одну переменную
-    square = sqrt(semiperimeter * pow(side2, 3)); // вычисление площади
-    cout << endl << setw(10) << left << "Сторона" << setw(10) << left << "Площадь"; // вывод результата
-    cout << endl << setw(10) << left << side << setw(10) << left << square;
+    double side = perimeter / 3; // сторона
+    double semiperimeter = perimeter / 2; // полупериметр
+    double square = sqrt(semiperimeter * pow(semiperimeter - side, 3)); // вычисление площади
+    wcout << setw(10) << left << L"Сторона" << setw(10) << left << L"Площадь" << endl; // вывод результата
+    wcout << setw(10) << left << side << setw(10) << left << square;
     return 0;
 }
